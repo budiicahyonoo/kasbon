30407b523508779936ce9c7f972408c0c24ae7ca
budichyn2003 <budigubernur15@gmail.com>
2026-08-13 03:20:36 +0700

    add package

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type DebtType = 'owed_to_me' | 'i_owe'
+
+export interface Debt {
+  id: string
+  user_id: string
+  type: DebtType
+  counterpart_name: string
+  amount: number
+  note: string | null
+  due_date: string | null
+  settled_at: string | null
+  created_at: string
+  updated_at: string
+}
